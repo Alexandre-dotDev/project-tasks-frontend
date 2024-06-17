cbb7b7dec612c24a173905dd13c29d08e46f58fa
Alexandre JS <alexandre.centro@yahoo.com.br>
2024-06-17 13:16:42 -0300

    refactor: access lifecycle methods in Fuction Components

--- src/App.js
+++ src/App.js
@@ -1,105 +1,120 @@
-// import { useState } from "react";
-import React from "react";
+// import React from "react";
+import { useState, useRef, useEffect } from "react";
 import TaskItem from "./components/TaskItem";
 
 // Component of the Class
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChangeMessage = this.handleChangeMessage.bind(this);
-        this.handleStateTasks = this.handleStateTasks.bind(this);
-        this.state = {
-            tasks: [
-                {
-                    id: "1",
-                    description: "Estudar Programação",
-                    isCompleted: false,
-                },
-                {
-                    id: "2",
-                    description: "Estudar Lógica",
-                    isCompleted: true,
-                },
-            ],
-            message: "Hello world in class",
-        };
-    }
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleChangeMessage = this.handleChangeMessage.bind(this);
+//         this.handleStateTasks = this.handleStateTasks.bind(this);
+//         this.state = {
+//             tasks: [
+//                 {
+//                     id: "1",
+//                     description: "Estudar Programação",
+//                     isCompleted: false,
+//                 },
+//                 {
+//                     id: "2",
+//                     description: "Estudar Lógica",
+//                     isCompleted: true,
+//                 },
+//             ],
+//             message: "Hello world in class",
+//         };
+//     }
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log(prevState);
-        console.log("Component was updated");
-    }
-    handleChangeMessage = () => {
-        this.setState({
-            message: "Olá mundo na classe",
-        });
-    };
+//     componentDidUpdate(prevProps, prevState) {
+//         console.log(prevState);
+//         console.log("Component was updated");
+//     }
+//     handleChangeMessage = () => {
+//         this.setState({
+//             message: "Olá mundo na classe",
+//         });
+//     };
 
-    handleStateTasks() {
-        this.setState({
-            tasks: [],
-        });
-    }
+//     handleStateTasks() {
+//         this.setState({
+//             tasks: [],
+//         });
+//     }
 
-    render() {
-        return (
-            <>
-                <div>
-                    <div>
-                        <p>{this.state.message}</p>
-                        <button onClick={this.handleChangeMessage}>
-                            Chance message
-                        </button>
-                    </div>
-                    {this.state.tasks.map((task) => (
-                        <>
-                            <TaskItem key={task.id} props={task} />
-                            <p>
-                                {task.isCompleted ? "Completo" : "Não completo"}
-                            </p>
-                        </>
-                    ))}
-                    <button onClick={this.handleStateTasks}>Clear task</button>
-                </div>
-            </>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <>
+//                 <div>
+//                     <div>
+//                         <p>{this.state.message}</p>
+//                         <button onClick={this.handleChangeMessage}>
+//                             Chance message
+//                         </button>
+//                     </div>
+//                     {this.state.tasks.map((task) => (
+//                         <>
+//                             <TaskItem key={task.id} props={task} />
+//                             <p>
+//                                 {task.isCompleted ? "Completo" : "Não completo"}
+//                             </p>
+//                         </>
+//                     ))}
+//                     <button onClick={this.handleStateTasks}>Clear task</button>
+//                 </div>
+//             </>
+//         );
+//     }
+// }
 
 // Component of the Function
-// const App = () => {
-//     const [message, setMessage] = useState("Hello world!");
-//     const [tasks, setTasks] = useState([
-//         {
-//             id: "1",
-//             description: "Estudar Programação",
-//             isCompleted: false,
-//         },
-//         {
-//             id: "2",
-//             description: "Estudar Lógica",
-//             isCompleted: true,
-//         },
-//     ]);
+const App = () => {
+    const mounted = useRef(false);
 
-//     const handleChangeMessage = () => {
-//         setMessage("Olá mundo");
-//     };
-//     return (
-//         <>
-//             <h1>{message}</h1>
-//             <button onClick={handleChangeMessage}>Change message</button>
+    useEffect(() => {
+        if (mounted.current === false) {
+            mounted.current = true;
+        } else {
+            console.log(`Component was updated with component function`);
+        }
+    });
 
-//             <div>
-//                 {tasks.map((task) => (
-//                     <>
-//                         <TaskItem key={task.id} props={task} />
-//                         <p>{task.isCompleted ? "Completo" : "Não completo"}</p>
-//                     </>
-//                 ))}
-//             </div>
-//         </>
-//     );
-// };
+    const [message, setMessage] = useState("Hello world!");
+    const [tasks, setTasks] = useState([
+        {
+            id: "1",
+            description: "Estudar Programação",
+            isCompleted: false,
+        },
+        {
+            id: "2",
+            description: "Estudar Lógica",
+            isCompleted: true,
+        },
+    ]);
+
+    const handleChangeMessage = () => {
+        setMessage("Olá mundo");
+    };
+
+    const handleClearTasks = () => {
+        setTasks([]);
+    };
+    return (
+        <>
+            <h1>{message}</h1>
+            <button onClick={handleChangeMessage}>Change message</button>
+
+            <div>
+                {tasks.map((task) => (
+                    <>
+                        <TaskItem key={task.id} props={task} />
+                        <p>{task.isCompleted ? "Completo" : "Não completo"}</p>
+                    </>
+                ))}
+                <button onClick={handleClearTasks}>Clear tasks</button>
+            </div>
+        </>
+    );
+};
 
 export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,32 +1,40 @@
+import { useEffect } from "react";
+
 // Component of the Function
-// const TaskItem = ({ props }) => {
-//     return (
-//         <>
-//             <h1>{props.description}</h1>
-//             <p>{props.isCompleted}</p>
-//         </>
-//     );
-// };
+const TaskItem = ({ props }) => {
+    useEffect(() => {
+        console.log("Component was mounted with component function");
+        return () => {
+            console.log("I will unmounted with component");
+        };
+    }, []);
+    return (
+        <>
+            <h1>{props.description}</h1>
+            <p>{props.isCompleted}</p>
+        </>
+    );
+};
 
-import React from "react";
+// import React from "react";
 
-// Component of the Class
-class TaskItem extends React.Component {
-    componentDidMount() {
-        console.log("Component was mounted");
-    }
-    componentWillUnmount() {
-        console.log("I will unmounted");
-    }
-    render() {
-        const { props } = this.props;
-        return (
-            <>
-                <h1>{props.description}</h1>
-                <p>{props.isCompleted}</p>
-            </>
-        );
-    }
-}
+// // Component of the Class
+// class TaskItem extends React.Component {
+//     componentDidMount() {
+//         console.log("Component was mounted");
+//     }
+//     componentWillUnmount() {
+//         console.log("I will unmounted");
+//     }
+//     render() {
+//         const { props } = this.props;
+//         return (
+//             <>
+//                 <h1>{props.description}</h1>
+//                 <p>{props.isCompleted}</p>
+//             </>
+//         );
+//     }
+// }
 
 export default TaskItem;
